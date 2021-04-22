3c861efa97b92cecde8a6e785b77b7294bc14006
shazx06 <shazibrahman@outlook.com>
2021-04-22 10:50:03 +0530

    made UI tweaks

--- algorithm_visualizer.py
+++ algorithm_visualizer.py
@@ -8,6 +8,7 @@ from mergesort import merge_sort
 from selectionSort import selection
 from insertionsort import insertionSort
 root = Tk()
+root.resizable(0,0)
 root.title('Sorting Algorithm Visualisation')
 root.maxsize(900, 600)
 root.config(bg='black')
@@ -26,7 +27,8 @@ def drawData(data, colorArray):
     x_width = c_width / (len(data) + 3)
     offset = 30
     spacing = 10
-    normalizedData = [i / max(data) for i in data]
+    max_data=max(data)
+    normalizedData = [i / max_data  for i in data]
     for i, height in enumerate(normalizedData):
         # top left
         x0 = i * x_width + offset + spacing
@@ -43,7 +45,8 @@ def drawData(data, colorArray):
 
 def Generate():
     global data
-    data.clear()
+    if data:
+        data.clear()
 
     minVal = int(minEntry.get())
     maxVal = int(maxEntry.get())
@@ -52,14 +55,11 @@ def Generate():
         messagebox.showwarning('Warning','Differnce of Max Value and Min Value is smaller than the Data Size') # needed condition otherwise , result in infinite while loop later
         return 
 
-
     for _ in range(size):
         v=random.randrange(minVal,minVal+1)
         while v in data: # needed loop to not add duplicates in the list . 
-            v=random.randrange(minVal,maxVal+1)
-        
+            v=random.randrange(minVal,maxVal+1)  
         data.append(v)
-
     drawData(data, ['red' for x in range(len(data))])  # ['red', 'red' ,....]
 
 
@@ -87,8 +87,8 @@ def StartAlgorithm():
 
 
 # frame / base lauout
-UI_frame = Frame(root, width=800, height=300, bg='grey')
-UI_frame.grid(row=0, column=0, padx=10, pady=5)
+UI_frame = Frame(root, height=300, bg='grey')
+UI_frame.grid(row=0, column=0, padx=10, pady=5,sticky='ew')
 
 canvas = Canvas(root, width=800, height=450, bg='white')
 canvas.grid(row=1, column=0, padx=10, pady=5)
@@ -96,32 +96,32 @@ canvas.grid(row=1, column=0, padx=10, pady=5)
 # User Interface Area
 # Row[0]
 Label(UI_frame, text="Algorithm: ", bg='grey').grid(
-    row=0, column=0, padx=5, pady=5, sticky=W)
+    row=0, column=0, padx=18, pady=5, sticky='ew')
 algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, values=[
                        'Bubble Sort', 'Quick Sort', 'Merge Sort', 'Selection Sort', 'Insertion Sort'])
-algMenu.grid(row=0, column=1, padx=5, pady=5)
-algMenu.current(0)
+algMenu.grid(row=0, column=1, padx=18, pady=5,sticky='ew')
+algMenu.current(2)
 
 speedScale = Scale(UI_frame, from_=0.1, to=5.0, length=200, digits=2,
                    resolution=0.2, orient=HORIZONTAL, label="Select Speed [s]")
-speedScale.grid(row=0, column=2, padx=5, pady=5)
+speedScale.grid(row=0, column=2, padx=18, pady=5)
 Button(UI_frame, text="Start", command=StartAlgorithm,
-       bg='red').grid(row=0, column=3, padx=5, pady=5)
+       bg='red').grid(row=0, column=3, padx=18, pady=5)
 
 # Row[1]
 sizeEntry = Scale(UI_frame, from_=3, to=25, resolution=1,
                   orient=HORIZONTAL, label="Data Size")
-sizeEntry.grid(row=1, column=0, padx=5, pady=5)
+sizeEntry.grid(row=1, column=0, padx=18, pady=5)
 
 minEntry = Scale(UI_frame, from_=0, to=10, resolution=1,
                  orient=HORIZONTAL, label="Min Value")
-minEntry.grid(row=1, column=1, padx=5, pady=5)
+minEntry.grid(row=1, column=1, padx=18, pady=5)
 
 maxEntry = Scale(UI_frame, from_=10, to=100, resolution=1,
                  orient=HORIZONTAL, label="Max Value")
-maxEntry.grid(row=1, column=2, padx=5, pady=5)
+maxEntry.grid(row=1, column=2, padx=18, pady=5)
 
 Button(UI_frame, text="Generate", command=Generate,
-       bg='white').grid(row=1, column=3, padx=5, pady=5)
+       bg='white').grid(row=1, column=3, padx=18, pady=5)
 
 root.mainloop()
